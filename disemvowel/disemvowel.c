1e50752a9b46a555197ea0f0d4477560e986b2ec
El Anderson <and10393@umn.edu>
2025-09-18 11:31:48 -0500

    Implement disemvowel
    Implements disemvowel to return and print a copy of a string minus the vowels it contains.
    
    Most tests pass.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,40 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+bool is_vowel(char c) {
+  c = tolower(c);
+  bool isVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+  return isVowel;
 }
+
+char *disemvowel(char *str) {
+  char *tmp;
+  char *string;
+  size_t size = 0;
+  // Allocate memory for length of string
+  // for length of string 
+  tmp = (char*) malloc (strlen(str) + 1);
+  for (size_t i = 0; i < strlen(str); i++) {
+  // if is_vowel(string[i]), add to temp array
+    if (!is_vowel(str[i])) {
+      tmp[size] = str[i];
+      size++;
+    }
+  }
+
+  string = (char*) malloc (size + 1);
+  // string = tmp;
+  strncpy(string, tmp, size + 1);
+  free(tmp);
+  return (char*) string;
+}
+
+
+// g++ -Wall -g -o disemvowel_test disemvowel_test.cpp disemvowel.c -lgtest -pthread -std=c++0x
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -5,12 +5,17 @@
 
 int main(int argc, char *argv[]) {
   char *line;
+  char *answer;
   size_t size;
   
   size = 100;
   line = (char*) malloc (size + 1);
 
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    answer = disemvowel(line);
+    printf("%s\n", answer);
+    // free(answer);
   }
+
+  free(line);
 }
